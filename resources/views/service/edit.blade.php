7004c57a10a0cb3557d24c6d97d6f38a40b31108
petermanoukian <thewizard_it@hotmail.com>
2018-01-01 13:12:27 +0400

    Update edit.blade.php

--- resources/views/service/edit.blade.php
+++ resources/views/service/edit.blade.php
@@ -13,7 +13,7 @@
 
 <div class ='full div1'>Name
 
-<input type="text" name="service_name" value = {{$service->service_name}}
+<input type="text" name="service_name" value = "{{$service->service_name}}"
 class='input'>
 {{$service->service_id}}
 {{$service->service_name}}
